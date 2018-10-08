2e9b634499e967ef9b47165b4099366fcfe8e358
jerome doucet <jerdct@gmail.com>
2018-10-08 14:29:20 +0200

    add mockito tests

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -1,14 +1,55 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.runner.RunWith;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestAlarm {
 
-    @Test
-    public void foo() {
-        Alarm alarm = new Alarm();
-        assertEquals(false, alarm.isAlarmOn());
-    }
+  @Mock
+  private Sensor mockedSensor;
+
+  @InjectMocks
+  private Alarm alarm = new Alarm();
+
+  @Test
+  public void AlarmOnWhenPresureLessThan17() {
+    // given
+    when(mockedSensor.popNextPressurePsiValue()).thenReturn(16d);
+
+    // when
+    alarm.check();
+
+    // then
+    assertEquals(true, alarm.isAlarmOn());
+  }
+
+  @Test
+  public void AlarmOnWhenPresureMoreThan21() {
+    // given
+    when(mockedSensor.popNextPressurePsiValue()).thenReturn(22d);
+
+    // when
+    alarm.check();
+
+    // then
+    assertEquals(true, alarm.isAlarmOn());
+  }
+
+  @Test
+  public void AlarmOffwhenPresureBetween17And21() {
+    // given
+    when(mockedSensor.popNextPressurePsiValue()).thenReturn(19d);
+ 
+    // when
+    alarm.check();
+
+    // then
+    assertEquals(false, alarm.isAlarmOn());
+  }
 }
